861b14f22aa87142574ff492a53a800a24a923d3
gailespns <146345485+gailespns@users.noreply.github.com>
2024-03-01 03:27:12 +0800

    Update bmi_calculator.py

--- day-02/TASKS/bmi_calculator.py
+++ day-02/TASKS/bmi_calculator.py
@@ -11,29 +11,31 @@ ABOVE 30.0          OBESITY
 # Write the code ↓ to read user's height and weight.
 # Be cautious when reading input of various data types.
 
+print("\n")
 
-
-
-
-
-
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
 
 
 # Write the code ↓ to perform the calculations of user's BMI and categorize its status.
 
+BMI = (weight / (height**2))
 
-
-
-
+if BMI < 18.5:
+    NUTRITIONAL_STATUS = ("UNDERWEIGHT")
+elif BMI > 18.5 and BMI < 24.9:
+    NUTRITIONAL_STATUS = ("NORMAL WEIGHT")
+elif BMI > 25 and BMI < 29.9:
+    NUTRITIONAL_STATUS = ("OVERWEIGHT")
+else:
+    NUTRITIONAL_STATUS = ("OBESITY")
 
 
 # Write the code ↓ to display the user's BMI and its classification.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 # Use :.2f format specifier when printing the BMI value to display the BMI with only two decimal places.
 
+print("\n")
 
-
-
-
-
-
+print("HEIGHT: ", height, "-", "WEIGHT: ", weight)
+print("BMI: {:.2f}".format(BMI), "-", "NUTRITIONAL STATUS: ", NUTRITIONAL_STATUS)
