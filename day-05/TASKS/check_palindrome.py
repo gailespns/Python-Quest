5dc4ac7cf2833491851c0104c553168eeb07e5e1
gailespns <146345485+gailespns@users.noreply.github.com>
2024-03-01 06:17:00 +0800

    Update check_palindrome.py

--- day-05/TASKS/check_palindrome.py
+++ day-05/TASKS/check_palindrome.py
@@ -1,25 +1,31 @@
 # Write the code ↓ to prompt the user to enter a word.
 # Be cautious when reading input of various data types.
 
+print("\n")
 
+print("PALINDROME CHECKER FOR ALF!")
 
+print("\n")
 
-
+word = input("Please, enter a word/s to check: ")
+wordLower = word.lower()
 
 
 # Write the code ↓ to check if the entered word is a palindrome.
 # Utilize string functions to compare the original word with its reverse.
 
+reverse = ""
 
-
-
+for char in wordLower:
+    reverse = char + reverse
 
 
 # Write the code ↓ to display whether the entered word is a palindrome or not.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
 
+print("\n")
 
-
-
-
-
+if wordLower == reverse:
+    print(f"{word} is a palindrome.")
+else:
+    print(f"{word} is not a palindrome.")
