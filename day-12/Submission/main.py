1746a77daaa02ebb1e2e164077855d557787f626
gailespns <146345485+gailespns@users.noreply.github.com>
2024-03-01 20:00:46 +0800

    Update main.py

--- day-12/Submission/main.py
+++ day-12/Submission/main.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 my_info = {
     "name": "Your Name",
     "course_and_section": "Your course and section",
-    "favorite_programming language": "Your programming language",
+    "favorite_programming_language": "Your programming language",
     "aws_service": "Give one AWS service you know"
 }
 
 @app.route('/', methods=['GET'])
 def get_info():
-    pass
+    return jsonify(my_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
